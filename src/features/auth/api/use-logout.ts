14f4ad3ee55a8f3b5de17e770966b742422ceb61
Sebastian-Nyberget <nybesebe@gmail.com>
2024-11-19 09:34:11 +0100

    Workspace members

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -28,7 +28,8 @@ export const useLogout = () => {
       onSuccess: () => {
         toast.success("Successfully logged out");
         router.refresh();
-        queryClient.invalidateQueries({ queryKey: ["current"] })
+        queryClient.invalidateQueries({ queryKey: ["current"] });
+        queryClient.invalidateQueries({ queryKey: ["workspaces"] });
       },
       onError: () => {
         toast.error("Failed to log out");
