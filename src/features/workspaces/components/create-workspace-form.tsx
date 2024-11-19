8b4f57d45963a401bc5adb2137cdfc1701c38094
Sebastian-Nyberget <nybesebe@gmail.com>
2024-11-19 12:08:39 +0100

    Responsive create-workspace modal + nuqs, react-use

--- src/features/workspaces/components/create-workspace-form.tsx
+++ src/features/workspaces/components/create-workspace-form.tsx
@@ -23,12 +23,14 @@ import { Input } from "@/components/ui/input";
 
 import { createWorkspaceSchema } from "../schemas";
 import { useCreateWorkspace } from "../api/use-create-workspace";
+import { useRouter } from "next/navigation";
  
 interface CreateWorkspaceFormProps {
   onCancel?: () => void;
 };
 
 export const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormProps) => {
+  const router = useRouter();
   const { mutate, isPending } = useCreateWorkspace();
 
   const inputRef = useRef<HTMLInputElement>(null);
@@ -47,9 +49,9 @@ export const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormProps) => {
     };
 
     mutate({ form: finalValues }, {
-      onSuccess: () => {
+      onSuccess: ({ data }) => {
         form.reset();
-        // TODO: Redirect to new workspace
+        router.push(`/workspaces/${data.$id}`)
       }
     });
   };
