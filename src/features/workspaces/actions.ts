a28684899a7ac9ecbfe7859080b74e2b302c3521
nightspirit07 <nybesebe@gmail.com>
2024-11-19 20:27:53 +0100

    Workspace Settings

--- src/features/workspaces/actions.ts
+++ src/features/workspaces/actions.ts
@@ -1,11 +1,13 @@
-"use server";
-
 import { cookies } from "next/headers";
 import { Databases, Client, Query, Account } from "node-appwrite";
 
 import { AUTH_COOKIE } from "@/features/auth/constants";
+import { getMember } from "@/features/members/utils";
 
 import { DATABASE_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
+
+import { Workspace } from "./types";
+
 
 export const getWorkspaces = async () => {
   try {
@@ -40,11 +42,52 @@ export const getWorkspaces = async () => {
       [
         Query.orderDesc("$createdAt"),
         Query.contains("$id", workspaceIds)
-      ]
+      ],
     );
 
     return workspaces;
   } catch {
     return { documents: [], total: 0 };
+  }
+};
+
+interface getWorkspaceProps {
+  workspaceId: string
+};
+
+export const getWorkspace = async ({ workspaceId }: getWorkspaceProps) => {
+  try {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
+
+  const session = cookies().get(AUTH_COOKIE)
+
+  if (!session) return null;
+
+    client.setSession(session.value)    
+    const databases = new Databases(client);
+    const account = new Account(client);
+    const user = await account.get();
+
+    const member = await getMember({
+      databases,
+      userId: user.$id,
+      workspaceId,
+    });
+
+    if (!member) {
+      return null;
+    }
+
+    const workspaces = await databases.getDocument<Workspace>(
+      DATABASE_ID,
+      WORKSPACES_ID,
+      workspaceId,
+    );
+
+    return workspaces;
+  } catch {
+    return null;
   }
 };
