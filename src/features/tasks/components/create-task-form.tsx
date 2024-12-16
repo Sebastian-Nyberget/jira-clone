4000e120c2e03f1926c86053d79f95bf21891662
Sebastian-Nyberget <nybesebe@gmail.com>
2024-12-16 09:47:27 +0100

    Task form

---
+++ src/features/tasks/components/create-task-form.tsx
@@ -0,0 +1,129 @@
+"use client";
+
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { 
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form"
+import { Separator } from "@/components/ui/separator";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { DatePicker } from "@/components/date-picker";
+
+import { createTaskSchema } from "../schemas"
+import { useCreateTask } from "../api/use-create-task";
+ 
+interface CreateTaskFormProps {
+  onCancel?: () => void;
+  projectOptions: { id: string, name: string, imageUrl: string }[];
+  memberOptions: { id: string, name: string }[];
+};
+
+export const CreateTaskForm = ({ onCancel, projectOptions, memberOptions }: CreateTaskFormProps) => {
+  const workspaceId = useWorkspaceId();
+  const router = useRouter();
+  const { mutate, isPending } = useCreateTask();
+
+  const form = useForm<z.infer<typeof createTaskSchema>>({
+    resolver: zodResolver(createTaskSchema.omit({ workspaceId: true })),
+    defaultValues: {
+        workspaceId
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof createTaskSchema>) => {
+    mutate({ form: {...values, workspaceId} }, {
+      onSuccess: () => {
+        form.reset();
+        // TODO: Redirect to new task
+      }
+    });
+  };
+
+  return(
+    <Card className="w-full h-full border-none shadow-none">
+      <CardHeader className="flex p-7">
+        <CardTitle className="text-xl font-bold">
+          Create a new task
+        </CardTitle>
+      </CardHeader>
+      <div className="px-7">
+        <Separator />
+      </div>
+      <CardContent className="p-7">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-y-4">
+              <FormField 
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                        Task Name
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field} 
+                        placeholder="Enter task name"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField 
+                control={form.control}
+                name="dueDate"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                        Due Date
+                    </FormLabel>
+                    <FormControl>
+                      <DatePicker {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="py-7">
+              <Separator />
+            </div>
+            <div className="flex items-center justify-between">
+              <Button
+                type="button"
+                size="lg"
+                variant="secondary"
+                onClick={onCancel}
+                disabled={isPending}
+                className={cn(!onCancel && "invisible")} 
+              >
+                Cancel
+              </Button>
+              <Button
+                disabled={isPending}
+                type="submit"
+                size="lg"
+              >
+                Create Task
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  )
+};
