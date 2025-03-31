713667fa2ac9a218d261492f6d898eb48e483c7a
nightspirit07 <144121816+Sebastian-Nyberget@users.noreply.github.com>
2025-03-31 14:30:52 +0200

    Data kanban

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Loader, Plus } from "lucide-react";
-import { useQueryState } from "nuqs"; 
+import { useQueryState } from "nuqs";
 
 import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 
@@ -15,31 +15,24 @@ import { DataFilters } from "./data-filters";
 import { useTaskFilters } from "../hooks/use-task-filters";
 import { DataTable } from "./data-table";
 import { columns } from "./columns";
+import { DataKanban } from "./data-kanban";
 
 export const TaskViewSwitcher = () => {
-  const [{
-    status,
-    assigneeId,
-    projectId,
-    dueDate
-  }] = useTaskFilters();
+  const [{ status, assigneeId, projectId, dueDate }] = useTaskFilters();
 
   const [view, setView] = useQueryState("task-view", {
     defaultValue: "table",
   });
 
-  const workspaceId = useWorkspaceId()
-  const { 
-    data: tasks, 
-    isLoading: isLoadingTasks 
-  } = useGetTasks({
+  const workspaceId = useWorkspaceId();
+  const { data: tasks, isLoading: isLoadingTasks } = useGetTasks({
     workspaceId,
     projectId,
     assigneeId,
     status,
-    dueDate
-  })
-  const { open } = useCreateTaskModal()
+    dueDate,
+  });
+  const { open } = useCreateTaskModal();
 
   return (
     <Tabs
@@ -50,55 +43,42 @@ export const TaskViewSwitcher = () => {
       <div className="h-full flex flex-col overflow-auto p-4">
         <div className="flex flex-col gap-y-2 lg:flex-row justify-between items-center">
           <TabsList className="w-full lg:w-auto">
-            <TabsTrigger
-              className="h-8 w-full lg:w-auto"
-              value="table"
-            >
+            <TabsTrigger className="h-8 w-full lg:w-auto" value="table">
               Table
             </TabsTrigger>
-            <TabsTrigger
-              className="h-8 w-full lg:w-auto"
-              value="kanban"
-            >
+            <TabsTrigger className="h-8 w-full lg:w-auto" value="kanban">
               Kanban
             </TabsTrigger>
-            <TabsTrigger
-              className="h-8 w-full lg:w-auto"
-              value="calendar"
-            >
+            <TabsTrigger className="h-8 w-full lg:w-auto" value="calendar">
               Calendar
             </TabsTrigger>
           </TabsList>
-          <Button
-            onClick={open}
-            size="sm"
-            className="w-full lg:w-auto"
-          >
+          <Button onClick={open} size="sm" className="w-full lg:w-auto">
             <Plus />
             New
           </Button>
         </div>
         <Separator className="my-4" />
-         <DataFilters />
+        <DataFilters />
         <Separator className="my-4" />
         {isLoadingTasks ? (
           <div className="w-full border rounded-lg h-[200px] flex flex-col items-center justify-center">
             <Loader className="size-5 animate-spin text-muted-foreground" />
           </div>
-        ): (
-        <>
-          <TabsContent value="table" className="mt-0">
-            <DataTable columns={columns} data={tasks?.documents ?? []} />
-          </TabsContent>
-          <TabsContent value="kanban" className="mt-0">
-            {JSON.stringify(tasks)}
-          </TabsContent>
-          <TabsContent value="calendar" className="mt-0">
-            {JSON.stringify(tasks)}
-          </TabsContent>
-        </>
+        ) : (
+          <>
+            <TabsContent value="table" className="mt-0">
+              <DataTable columns={columns} data={tasks?.documents ?? []} />
+            </TabsContent>
+            <TabsContent value="kanban" className="mt-0">
+              <DataKanban data={tasks?.documents ?? []} />
+            </TabsContent>
+            <TabsContent value="calendar" className="mt-0">
+              {JSON.stringify(tasks)}
+            </TabsContent>
+          </>
         )}
       </div>
     </Tabs>
   );
-}
+};
