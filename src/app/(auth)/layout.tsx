30ad0a034f630e39f955ef6831f543ed6e16d1f8
Sebastian-Nyberget <nybesebe@gmail.com>
2025-04-01 17:04:47 +0200

    Change logo

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,36 +1,36 @@
 "use client";
 
 import Image from "next/image";
-import Link from 'next/link';
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 
 interface AuthLayoutProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
-    const pathname = usePathname();
-    const isSignedIn = pathname === "/sign-in";
+  const pathname = usePathname();
+  const isSignedIn = pathname === "/sign-in";
 
-    return ( 
-        <main className="bg-neutral-100 min-h-screen">
-          <div className="mx-auto max-w-screen-2xl p-4">
-            <nav className="flex justify-between items-center">
-              <Image src="/logo.svg" alt="Logo" width={152} height={56} />
-                <Button asChild variant="secondary">
-                  <Link href={isSignedIn ? "/sign-up" : "/sign-in"}>
-                    {isSignedIn ? "Sign Up" : "Login"}
-                  </Link>
-                </Button>
-            </nav>
-            <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
-              {children}
-            </div>
-          </div>
-        </main>
-     );
-}
- 
+  return (
+    <main className="bg-neutral-100 min-h-screen">
+      <div className="mx-auto max-w-screen-2xl p-4">
+        <nav className="flex justify-between items-center">
+          <Image src="/logo.png" alt="Logo" width={228} height={84} />
+          <Button asChild variant="secondary">
+            <Link href={isSignedIn ? "/sign-up" : "/sign-in"}>
+              {isSignedIn ? "Sign Up" : "Login"}
+            </Link>
+          </Button>
+        </nav>
+        <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
+          {children}
+        </div>
+      </div>
+    </main>
+  );
+};
+
 export default AuthLayout;
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -7,18 +7,17 @@ import { WorkspaceSwitcher } from "./workspace-switcher";
 import { Projects } from "./projects";
 
 export const Sidebar = () => {
-    return ( 
-        <aside className="h-full bg-neutral-100 p-4 w-full">
-          <Link href="/">
-            <Image src="/logo.svg" alt="Logo" width={164} height={48} />
-          </Link>
-          <Separator className="my-4" />
-          <WorkspaceSwitcher />
-          <Separator className="my-4" />
-          <Navigation />
-          <Separator className="my-4" />
-          <Projects />
-        </aside>
-     );
-}
- 
+  return (
+    <aside className="h-full bg-neutral-100 p-4 w-full">
+      <Link href="/">
+        <Image src="/logo.png" alt="Logo" width={246} height={72} />
+      </Link>
+      <Separator className="my-4" />
+      <WorkspaceSwitcher />
+      <Separator className="my-4" />
+      <Navigation />
+      <Separator className="my-4" />
+      <Projects />
+    </aside>
+  );
+};
