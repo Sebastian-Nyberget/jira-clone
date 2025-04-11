610f7cea7081fc14d137ee9bb381ccbf933a8506
nightspirit07 <144121816+Sebastian-Nyberget@users.noreply.github.com>
2025-04-11 13:43:54 +0200

    Fix sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,14 @@ module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
   generateRobotsTxt: true,
   sitemapSize: 7000,
+  additionalPaths: async (config) => {
+    const result = [];
+
+    result.push({ loc: "/" });
+    result.push({ loc: "/sign-in" });
+    result.push({ loc: "/sign-up" });
+    result.push({ loc: "/workspaces" });
+
+    return result;
+  },
 };
